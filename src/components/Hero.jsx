7b856f6068e09c438e7ae61dc5b9ccdf43c8f82a
Era-smus <erasmussaat1@gmail.com>
2026-01-10 00:04:16 +0300

    Modified the hero page section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,12 @@
+import homeImg from '../assets/images/home.jpg'
+
 const Hero = () => {
   return (
-    <section id="hero" className="hero">
+    <section 
+      id="hero" 
+      className="hero"
+      style={{ backgroundImage: `url(${homeImg})` }}
+    >
       <div className="overlay"></div>
       <div className="hero-content">
         <h1>Your Academic Success Partner</h1>
